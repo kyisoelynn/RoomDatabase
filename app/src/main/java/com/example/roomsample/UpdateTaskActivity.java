b7ba950e4a79c6de6d6ad4f2e3e77bde82d0fe72
Kyi Soe Lynn <kyisoelynn@gmail.com>
2019-06-02 12:40:17 +0630

    RoomDatabase in complete state

--- app/src/main/java/com/example/roomsample/UpdateTaskActivity.java
+++ app/src/main/java/com/example/roomsample/UpdateTaskActivity.java
@@ -1,13 +1,19 @@
 package com.example.roomsample;
 
+import android.arch.persistence.room.Delete;
+import android.content.DialogInterface;
+import android.content.Intent;
+import android.os.AsyncTask;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.CheckBox;
 import android.widget.EditText;
 
 import com.example.roomsample.Entity.Task;
+import com.example.roomsample.database.DatabaseClient;
 
 public class UpdateTaskActivity extends AppCompatActivity {
 
@@ -34,9 +40,32 @@ public class UpdateTaskActivity extends AppCompatActivity {
                 updateTask(task);
             }
         });
+
+        findViewById(R.id.button_delete).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                AlertDialog.Builder alertDialog = new AlertDialog.Builder(UpdateTaskActivity.this);
+                alertDialog.setTitle("Are you sure to delete?");
+                alertDialog.setPositiveButton("Yes", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                        deleteTask(task);
+                    }
+                });
+                alertDialog.setNegativeButton("No", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+
+                    }
+                });
+
+                AlertDialog dialog = alertDialog.create();
+                dialog.show();
+            }
+        });
     }
 
-    public void loadTask(Task task) {
+    public void loadTask(final Task task) {
         editTextTask.setText(task.getTask());
         editTextDesc.setText(task.getDesc());
         editTextFinishBy.setText(task.getFinishby());
@@ -44,9 +73,76 @@ public class UpdateTaskActivity extends AppCompatActivity {
 
     }
 
-    public void updateTask(Task task) {
+    public void updateTask(final Task task) {
         final String sTask = editTextTask.getText().toString().trim();
         final String sDesc = editTextDesc.getText().toString().trim();
         final String sFinishBy = editTextFinishBy.getText().toString().trim();
+
+        if (sTask.isEmpty()) {
+            editTextTask.setError("Task required");
+            editTextTask.requestFocus();
+            return;
+        }
+
+        if (sDesc.isEmpty()) {
+            editTextDesc.setError("Description required");
+            editTextDesc.requestFocus();
+            return;
+        }
+
+        if (sFinishBy.isEmpty()) {
+            editTextFinishBy.setError("Finish By required");
+            editTextFinishBy.requestFocus();
+            return;
+        }
+
+        class UpdateTask extends AsyncTask<Void, Void, Void> {
+
+            @Override
+            protected Void doInBackground(Void... voids) {
+
+                task.setTask(sTask);
+                task.setDesc(sDesc);
+                task.setFinishby(sFinishBy);
+                task.setFinished(checkBoxFinished.isChecked());
+
+                DatabaseClient.getInstance(getApplicationContext()).getAppDatabase().taskDao().update(task);
+
+                return null;
+            }
+
+            @Override
+            protected void onPostExecute(Void aVoid) {
+                super.onPostExecute(aVoid);
+
+                finish();
+
+                startActivity(new Intent(UpdateTaskActivity.this, MainActivity.class));
+            }
+        }
+
+        UpdateTask updateTask = new UpdateTask();
+        updateTask.execute();
+    }
+
+    public void deleteTask(final Task task) {
+        class DeleteTask extends AsyncTask<Void, Void, Void> {
+
+            @Override
+            protected Void doInBackground(Void... voids) {
+
+                DatabaseClient.getInstance(getApplicationContext()).getAppDatabase().taskDao().delete(task);
+                return null;
+            }
+
+            @Override
+            protected void onPostExecute(Void aVoid) {
+                super.onPostExecute(aVoid);
+                startActivity(new Intent(UpdateTaskActivity.this, MainActivity.class));
+            }
+        }
+
+        DeleteTask deleteTask = new DeleteTask();
+        deleteTask.execute();
     }
 }
--- app/src/main/java/com/example/roomsample/adapter/TasksAdapter.java
+++ app/src/main/java/com/example/roomsample/adapter/TasksAdapter.java
@@ -1,19 +1,21 @@
 package com.example.roomsample.adapter;
 
 import android.content.Context;
+import android.content.Intent;
 import android.support.annotation.NonNull;
 import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
 
 import com.example.roomsample.Entity.Task;
 import com.example.roomsample.R;
-import com.example.roomsample.ViewHolder.TasksViewHolder;
+import com.example.roomsample.UpdateTaskActivity;
 
 import java.util.List;
 
-public class TasksAdapter extends RecyclerView.Adapter<TasksViewHolder> {
+public class TasksAdapter extends RecyclerView.Adapter<TasksAdapter.TasksViewHolder> {
 
     private Context mContext;
 
@@ -50,4 +52,31 @@ public class TasksAdapter extends RecyclerView.Adapter<TasksViewHolder> {
     public int getItemCount() {
         return taskList.size();
     }
+
+    public class TasksViewHolder extends RecyclerView.ViewHolder implements View.OnClickListener{
+
+        public TextView textViewStatus, textViewTask, textViewDesc, textViewFinishBy;
+
+        public TasksViewHolder(@NonNull View itemView) {
+            super(itemView);
+
+            textViewStatus = itemView.findViewById(R.id.textViewStatus);
+            textViewTask = itemView.findViewById(R.id.textViewTask);
+            textViewDesc = itemView.findViewById(R.id.textViewDesc);
+            textViewFinishBy = itemView.findViewById(R.id.textViewFinishBy);
+
+            itemView.setOnClickListener(this);
+        }
+
+        @Override
+        public void onClick(View view) {
+            Task task = taskList.get(getAdapterPosition());
+            Intent intent = new Intent(mContext, UpdateTaskActivity.class);
+            intent.putExtra("task", task);
+            mContext.startActivity(intent);
+
+        }
+    }
+
+
 }
